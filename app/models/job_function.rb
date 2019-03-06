f2c57a60d5b04ae3e5caf19fcf0b86e00c68e6bb
Giulia Goeller <giulia.goeller@gmx.de>
2019-03-06 17:07:23 +1100

    search adjusted

--- app/models/job_function.rb
+++ app/models/job_function.rb
@@ -1,21 +1,22 @@
 class JobFunction < ApplicationRecord
-
-  include PgSearch
-  pg_search_scope :global_search_job_function_and_user_characteristics,
-    against: [ :name ],
-    associated_against: {
-      activity: [ :description ],
-      availability: [ :day_of_week, :period_of_day ],
-      company: [ :name ],
-      industry: [ :name ],
-      user: [ :address, :radius ],
-      job_title: [ :name ],
-      professional_interest: [ :name ],
-      skill: [ :name ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
+  belongs_to :career_position
+  has_many :users, through: :career_position
+  # include PgSearch
+  # pg_search_scope :global_search_job_function_and_user_characteristics,
+  #   against: [ :name ],
+  #   associated_against: {
+  #     # activity: [ :description ],
+  #     # availability: [ :day_of_week, :period_of_day ],
+  #     # company: [ :name ],
+  #     # industry: [ :name ],
+  #     user: [ :address, :radius ],
+  #     # job_title: [ :name ],
+  #     # professional_interest: [ :name ],
+  #     # skill: [ :name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
   # multisearchable against: [ :name ]
   #   pg_search_scope :search_by_name,
   #     against: [ :name ],
