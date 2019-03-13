338a72b757b76f5cc246ae6ace99684c2aa107da
Jonas Japing <jonasjaping@gmail.com>
2019-03-14 09:48:57 +1100

    made current_user invisible on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,10 @@ class UsersController < ApplicationController
     filter_for_activity
     filter_for_skill
     filter_for_availability
-
+    # raise
     # See only users which have an address so that number of map markers corresponds to number of cards below
     @users = @users.where.not(latitude: nil, longitude: nil)
+    @users = @users.where.not(id: current_user.id)
 
     # Set markers to display on the map
     @markers = @users.map do |user|
