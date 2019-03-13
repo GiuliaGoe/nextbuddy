a13a7a6ff0933d3ac43db0fedd31a72d135e232e
Giulia Goeller <giulia.goeller@gmx.de>
2019-03-14 00:21:28 +1100

    search function working again

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,8 @@ class User < ApplicationRecord
   has_many :received_meetings, class_name: "Meeting", foreign_key: :recipient_id
   has_many :meetings
   has_many :career_positions
-  has_many :job_functions, through: :career_positions
   has_many :job_titles, through: :career_positions
   has_many :companies, through: :career_positions
-  has_many :industries, through: :career_positions
   has_many :activities
   has_many :availabilities
   has_many :professional_interests
@@ -41,8 +39,6 @@ class User < ApplicationRecord
   associated_against: {
     activities: [:description],
     companies: [:name],
-    industries: [:name],
-    job_functions: [:name],
     job_titles: [:name],
     professional_interests: [:name],
     skills: [:name]
